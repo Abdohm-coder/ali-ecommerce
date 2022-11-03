78ae068621dd7dd9534292787190efc875597dc8
Abdohm-coder <hadjmedabdo@gmail.com>
2022-11-03 22:39:19 +0100

    feat(read-more): add ReadMore component

---
+++ src/components/read-more.jsx
@@ -0,0 +1,13 @@
+import { BiChevronsDown } from "react-icons/bi";
+export default function ReadMore() {
+  return (
+    <div className=" w-full flex flex-col items-center justify-center space-y-3">
+      <strong className="text-xs text-light text-center w-full">
+        إقرأ المزيد
+      </strong>
+      <div className="moveUpDownAnimation text-light">
+        <BiChevronsDown size={30} />
+      </div>
+    </div>
+  );
+}
