238ca260d4c24fc0989cef39c931f79e4b809e1d
Abdohm-coder <hadjmedabdo@gmail.com>
2022-11-04 21:33:22 +0100

    feat assets schema

---
+++ src/data/assets.js
@@ -0,0 +1,72 @@
+export const assets = {
+  logo: {
+    dark_mode: "",
+    light_mode: "",
+  },
+  homepage: {
+    hero: {
+      images: [],
+      title: "",
+      offer_script: "",
+    },
+    features: {
+      title: "",
+      description: "",
+      items: [
+        {
+          id: "",
+          icon: "",
+          text: "",
+        },
+      ],
+    },
+    records: [
+      {
+        id: "",
+        icon: "",
+        text: "",
+      },
+    ],
+    feedbacks: {
+      title: "",
+      reviews: [
+        {
+          id: "",
+          avatar: "",
+          name: "",
+          text: "",
+        },
+      ],
+    },
+  },
+  customer_info: {
+    title: "",
+    description: "",
+    features: [
+      {
+        id: "",
+        icon: "",
+        text: "",
+      },
+    ],
+  },
+  order_info: {
+    title: "",
+    description: "",
+    note: "",
+    privacy_rules: "",
+  },
+  footer: {
+    logo: {
+      dark_mode: "",
+      light_mode: "",
+    },
+    items: [
+      {
+        id: "",
+        label: "",
+        text: "",
+      },
+    ],
+  },
+};
