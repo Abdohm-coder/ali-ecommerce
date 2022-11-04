e80f7f51b91177af23e18f2570908f5cd8d4f92a
Abdohm-coder <hadjmedabdo@gmail.com>
2022-11-04 16:15:49 +0100

    feat(order-info)

--- src/pages/OrderInfo.jsx
+++ src/pages/OrderInfo.jsx
@@ -1,65 +1,48 @@
+import { Link } from "react-router-dom";
 import OrderItem from "../components/order-item";
-
+import { ROUTES } from "../utils/routes";
 const data = {
-  order_details: [
-    {
-      label: "إسم المنتوج",
-      text: "وسادة نابوفا",
-    },
-    {
-      label: "سعر الواحدة",
-      text: "2900 2320 دج",
-    },
-    {
-      label: "الكمية",
-      text: "2",
-    },
-    {
-      label: "نوع العرض",
-      text: "وسادتين (2) بتخفيض 20%",
-    },
-    {
-      label: "اسم الزبون",
-      text: "sdsds",
-    },
-    {
-      label: "رقم الهاتف",
-      text: "0549848545",
-    },
-    {
-      label: "ولاية التوصيل",
-      text: "Alger - الجزائر العاصمة",
-    },
-    {
-      label: "بلدية التوصيل",
-      text: "Baraki - براقي",
-    },
-    {
-      label: "تكاليف الشحن",
-      text: "400 دج",
-    },
-  ],
+  product_name: "وسادة نابوفا",
+  offer_type: "وسادتين (2) بتخفيض 20%",
+  client_name: "sdsds",
+  client_phone: "0549848545",
+  client_state: "Alger - الجزائر العاصمة",
+  client_city: "Baraki - براقي",
+  shipping: "400 دج",
+  quantity: "2",
+  product_price: " 2900 دج",
   discount: true,
+  product_discount: "2320 دج",
   price_before: "6200 دج",
   price_total: "5040 دج",
 };
 function OrderInfo() {
   return (
-    <section className="pt-8">
+    <section className="pt-8 layout">
       <h1 className="text text-4xl font-black mb-2">تأكدوا من طلبكم</h1>
       <p className="text mb-4">
         تأكدو من المعلومات التي أدخلتموها و أضغطو على الزر أسفله حتى نرسل لكم
         رسالة نصية قصيرة و نقوم بتأكيد طلبيتكم
       </p>
-      <span className="rounded-md p-2 inline-flex dark:bg-btn-light/5 dark:text-white mb-4">
+      <span className="rounded-md p-3 inline-flex dark:bg-btn-light/5 dark:text-gray-100 mb-4">
         <strong>
           تكاليف الشحن تختلف حسب المكان الذي حددته، الأماكن الأبعد قد ترفع من
           تكلفة الشحن.
         </strong>
       </span>
-      {data.order_details.map((props, index) => (
-        <OrderItem key={index} {...props} />
-      ))}
+      <OrderItem label="اسم المنتوج" text={data.product_name} />
+      <OrderItem
+        label="سعر الواحدة"
+        text={data.product_price}
+        product_discount={data.product_discount}
+      />
+      <OrderItem label="الكمية" text={data.quantity} />
+      <OrderItem label="نوع العرض" text={data.offer_type} />
+      <OrderItem label="اسم الزبون" text={data.client_name} />
+      <OrderItem label="رقم الهاتف" text={data.client_phone} />
+      <OrderItem label="ولاية التوصيل" text={data.client_state} />
+      <OrderItem label="بلدية التوصيل" text={data.client_city} />
+      <OrderItem label="تكاليف الشحن" text={data.shipping} />
       {data.discount && (
         <OrderItem
           discount
@@ -67,7 +50,13 @@ function OrderInfo() {
           text={data.price_before}
         />
       )}
-      <OrderItem label="الإجمالي الكلي" text={data.price_total} />
+      <OrderItem label="الإجمالي الكلي" total_price text={data.price_total} />
+      <Link to={ROUTES.SUCCESS}>
+        <button className="btn">تأكيد الطلبية</button>
+      </Link>
+      <p className="text-light text-sm text-center max-w-[250px] mx-auto mt-2">
+        بضغطك على زر الطلب فأنت توافق على قوانين الاستخدام و بوليصة التأمين
+      </p>
     </section>
   );
 }
