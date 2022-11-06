93673621b1948b1cb071e05067a24beb7f076d8e
Abdohm-coder <hadjmedabdo@gmail.com>
2022-11-06 20:19:09 +0100

    feat facebook pixel component

--- src/dashboard/components/pixel.jsx
+++ src/dashboard/components/pixel.jsx
@@ -1,15 +1,64 @@
-import { Paper } from "@mantine/core";
+import { Button, Card, Paper, TextInput } from "@mantine/core";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Pixel() {
+  const [pixels, setPixels] = useState([""]);
+
+  const onSubmit = () => {
+    const filter = pixels.filter((pixel) => pixel !== "");
+    setPixels(filter);
+    // POST Facebook Pixel 
+  };
+
   return (
-    <Paper
-      shadow="xs"
-      radius={6}
-      className="flex flex-col flex-1 h-32 p-2 border-[1px] border-light">
-      <h3 className="text-dark">"فايسبوك بيكسل</h3>
-      <div className="grid place-items-center h-full flex-1 ">
-        <p className="text-4xl"></p>
-      </div>
-    </Paper>
+    <div className="grid md:grid-cols-2 ">
+      <Card shadow="xs" p="lg" radius={6} className="flex flex-col flex-1">
+        <h3 className="text-dark mb-4">فايسبوك بيكسل</h3>
+        <div className="flex justify-end">
+          <Button
+            variant="filled"
+            onClick={() => setPixels((pixel) => [...pixel, ""])}
+            className="hover:bg-btn-dark bg-btn-dark/90 ml-3">
+            إضافة
+          </Button>
+          <Button
+            variant="filled"
+            onClick={() =>
+              setPixels((pixel) => {
+                console.log(pixel.filter((p, i) => i !== pixel.length - 1));
+                return pixel.filter((p, i) => i !== pixel.length - 1);
+              })
+            }
+            className="hover:bg-red-500 bg-red-600">
+            حذف
+          </Button>
+        </div>
+        <div className="grid place-items-center h-full flex-1 ">
+          {pixels.map((pixel, index) => (
+            <TextInput
+              radius={6}
+              label="Id"
+              mb="sm"
+              value={pixel}
+              onChange={(e) => {
+                const id = e.target.value;
+                setPixels((pixel) =>
+                  pixel.map((p, i) => (i === index ? id : p))
+                );
+              }}
+              placeholder="أدخل فايسبوك بيكسل Id"
+              className="w-full"
+            />
+          ))}
+        </div>
+        <Button
+          variant="filled"
+          onClick={onSubmit}
+          className="hover:bg-green-500 bg-green-600">
+          حفظ
+        </Button>
+      </Card>
+    </div>
   );
 }
